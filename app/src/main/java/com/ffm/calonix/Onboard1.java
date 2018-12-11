55f643202de78bb604ca9cfc7511ea568be76345
Muhammad Abdurrahman <mmz.mhd@gmail.com>
2018-12-11 19:42:38 +0700

    v.0.0.0.0.1

---
+++ app/src/main/java/com/ffm/calonix/Onboard1.java
@@ -0,0 +1,13 @@
+package com.ffm.calonix;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Onboard1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboard1);
+    }
+}
